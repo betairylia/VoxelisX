48f40c6f513b56add9ace1248407c7f4f104372d
betairylia <betairya@gmail.com>
2026-05-04 14:34:46 +0900

    build safe

--- Runtime/Core/VoxelEntity.cs
+++ Runtime/Core/VoxelEntity.cs
@@ -2,16 +2,12 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
 using Unity.Collections.NotBurstCompatible;
 using Unity.Jobs;
 using Unity.Mathematics;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Serialization;
 
 namespace Voxelis
 {
